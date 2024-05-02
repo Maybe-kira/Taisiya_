6866654a5bf1881f9e13f64fd74f5601ef3344a2
1233taisiya <167587521+1233taisiya@users.noreply.github.com>
2024-05-03 00:53:20 +0300

    Update ربت الفل.js

--- plugins/ربت الفل.js
+++ plugins/ربت الفل.js
@@ -6,14 +6,14 @@ global.rpg = {
     if (isNaN(level)) return { name: '', level: '' }
     
     const role = [
-      { name: "جنية", level: 0 }, { name: "مبتدئ", level: 4 }, 
-      { name: "ساحر", level: 8 }, { name: "مغوار", level: 12 }, 
-      { name: "فارس", level: 16 }, { name: "مغامر", level: 20 }, 
-      { name: "سينسي", level: 24 }, { name: "محنك", level: 28 }, 
-      { name: "قرصان", level: 32 }, { name: "حكيم", level: 36 },
-      { name: "دوق", level: 48 }, { name: "الساحر الأسود", level: 52 }, 
-      { name: "صياد", level: 56 }, { name: "الملك", level: 60 }, 
-      { name: "ملك", level: 100 }
+      { name: "جنية🧚‍♀️", level: 0 }, { name: "مبتدئ", level: 4 }, 
+      { name: "ساحر🧙🏻", level: 8 }, { name: "مغوار", level: 12 }, 
+      { name: "فارس🏇🏻", level: 16 }, { name: "مغامر", level: 20 }, 
+      { name: "سينسي🪶", level: 24 }, { name: "محنك", level: 28 }, 
+      { name: "قرصان🏴‍☠️", level: 32 }, { name: "حكيم", level: 36 },
+      { name: "نخبة🎗️", level: 48 }, { name: "الساحر الأسود", level: 52 }, 
+      { name: "صياد🧜🏻‍♂️", level: 56 }, { name: "الملك", level: 60 }, 
+      { name: "ملك🫅", level: 100 }
     ];
 
     return role.reverse().find(role => level >= role.level)
